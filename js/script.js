bf74387e378f181a5ae31df1a3772a2e0506bb30
lijorajan <rajan.lijo@thinksys.com>
2021-07-21 23:06:00 +0530

    changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var app = angular.module('AngularJSMovies', []);
+
+app.controller('MoviesController', function ($scope, $http) {
+    $scope.akshaya = 'Akshaya';
+
+    $http.get('./assets/movies.json', { cache: true })
+        .then(function (res) {
+            $scope.movies = res.data;
+        });
+});
